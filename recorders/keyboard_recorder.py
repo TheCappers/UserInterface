df804c935fd53b451aa253a3433bc8c96c51c6ec
kmamamori <kmamamori@miners.utep.edu>
2021-10-04 08:38:01 -0600

    Adding the right code for keyboard_recorder.py

--- recorders/keyboard_recorder.py
+++ recorders/keyboard_recorder.py
@@ -1,78 +1,39 @@
-from pynput import mouse
+from pynput import keyboard
+from pynput.keyboard import Key, Controller
 from recorders.recorded_data import RecordedData
+from threading import Lock
 
-from Database.Database import DataBase
-'''
-Sets up listener. To start the listener invoke start() and to stop it invoke stop()
-'''
-class MouseRecorder(RecordedData):
+class KeyboardRecorder(RecordedData):
+		def __init__(self, isAutoRecord):
+			self.isAutoRecord = True
+			self.listener = keyboard.Listener(
+				on_press=self.on_press
+			)
+			self.startKeyboardRecording()
 
-    def __init__(self):
-        self.autorecording = True
-        self.__listener = mouse.Listener(
-            on_move=self.__on_move,
-            on_click=self.__on_click,
-            on_scroll=self.__on_scroll)
-        self.__listener.start()
-        self.mouse_movement = {"position": (0,0), "clicked": False}
+		def startKeyboardRecording(self):
+			# ...or, in a non-blocking fashion:
+			if self.isAutoRecord:
+					self.listener.start()
+			print('start keyboard recordig')
 
-    def __on_move(self, x, y):
-        self.mouse_movement['position'] = (x,y)
-        if self.autorecording:
-            print(self.mouse_movement)
+		def stopKeyboardRecording(self):
+			if not self.isAutoRecord:
+					self.listener.stop()
+			print('stop keyboard recording')
 
-    def __on_click(self, x, y, button, pressed):
-        if self.autorecording:
-            if pressed:
-                self.mouse_movement['clicked'] = True
-            else:
-                self.mouse_movement['clicked'] = False
+		def on_press(self, key):
+				try:
+						print('alphanumeric key {0} pressed'.format(
+								key.char))
+						self.get_timestamp()
+				except AttributeError:
+						print('special key {0} pressed'.format(
+								key))
 
-    def __on_scroll(self, x, y, dx, dy):
-        if self.autorecording:
-            print('Scrolled {0} at {1}'.format(
-                'down' if dy < 0 else 'up',
-                (x, y)))
-
-    def start(self):
-        if self.autorecording:
-           return 
-        self.autorecording = True
-
-    def stop(self):
-        if not self.autorecording:
-            return 
-        self.autorecording = False
-
-    def terminate(self):
-        self.__listener.stop()
-
-
-# This is for getting full recording data
-# r = RecordedData()
-# m = MouseRecorder()
-# r.recorded_data['type'] = "mouse_movement"
-# r.recorded_data['data'] = m.mouse_movement
-
-# print(r.recorded_data)
-
-# Schema for sending the data (keystroke)
-# post_1 = {
-#     "name": "Keystroke",
-#     "Keystroke": "H",
-#     "Date": "9/11/2021",
-#     "IP Address": "1.2.3.4",
-# }
-# Schema for sending the data (mouse action)
-# post_2 = {
-#     "name": "Mouse_Action",
-#     "coordinate": (200,402),
-#     "click": True,
-#     "date": "9/11/2021",
-#     "ip_address": "1.2.3.4",
-#     "mac_address": "39:0f:b2:29:48"
-# }
-
-# db = DataBase()
-# db.query_db("post", post_1, "")
-# print(db.query_db("find", post_1, ""))
+		def on_release(self, key):
+				print('{0} released'.format(
+						key))
+				if key == keyboard.Key.esc:
+						# Stop listener
+						return False
