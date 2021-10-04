097c668ffc884932b554feeb5ec8b85b6b52564b
dbxation <daamparan@miners.utep.edu>
2021-10-03 23:11:05 -0600

    Controller class with additional methods to be filled out

--- controller/controller.py
+++ controller/controller.py
@@ -19,3 +19,16 @@ class Controller:
         self.config.setThreshold(amount)
         full = self.config.storage_alert()
         return full  # send to view
+
+    def export(self, item):  # here is where we would use the database to export
+        return
+
+    def view(self, item):  # here is where we would connect to database to view an item in avert
+        return item
+
+    def searchDB(self, item):  # here is where we would connect to database to pull up results
+        # logic for how it will be viewed
+        return
+
+    def annotationAdd(self, annot, item): # here we update the item in the DB with an annotation
+        return
