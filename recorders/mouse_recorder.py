89c3995e607cbbc080b78688b1f738132521842f
eretana238 <eretana238@gmail.com>
2021-10-04 20:20:07 -0600

    Fixed missing _

--- recorders/mouse_recorder.py
+++ recorders/mouse_recorder.py
@@ -5,18 +5,18 @@ from Database.Database import DataBase
 '''
 Sets up listener. To start the listener invoke start() and to stop it invoke stop()
 '''
+
+
 class MouseRecorder(RecordedData):
 
     def __init__(self):
         RecordedData.__init__(self)
         self.__autorecording = True
-        
         self.__mouse_action = self.get_recorded_data()
         self.__mouse_action['name'] = 'Mouse_Action'
-        self.__mouse_action['data'] = {"position": (0,0), "clicked": False, "scroll": 0, "button": ''}
-
+        self.__mouse_action['data'] = {"position": (
+            0, 0), "clicked": False, "scroll": 0, "button": ''}
         self.__mock_db = []
-
         self.__mock = False
 
         self.__db = DataBase()
@@ -29,7 +29,7 @@ class MouseRecorder(RecordedData):
 
     def _on_move(self, x, y):
         if self.__autorecording:
-            self.__mouse_action['data']['position'] = (x,y)
+            self.__mouse_action['data']['position'] = (x, y)
             self.insert_to_db()
 
     def _on_click(self, x, y, button, pressed):
@@ -54,12 +54,12 @@ class MouseRecorder(RecordedData):
 
     def start(self):
         if self.__autorecording:
-           return 
+            return
         self.__autorecording = True
 
     def stop(self):
         if not self.__autorecording:
-            return 
+            return
         self.__autorecording = False
 
     def terminate(self):
@@ -67,40 +67,11 @@ class MouseRecorder(RecordedData):
 
     def insert_to_db(self):
         if not self.__mock:
-            self.__db.query_db("post", self._mouse_action, "")
+            self.__db.query_db("post", self.__mouse_action, "")
         else:
-            self.__mock_db.append(self._mouse_action)
+            self.__mock_db.append(self.__mouse_action)
 
     def print_mock_db(self):
         if self.__mock:
-            print("first value stored: {0}\nstored items: {1}".format(self._mock_db[-1], len(self._mock_db)))
-
-
-# This is for getting full recording data
-# r = RecordedData()
-# m = MouseRecorder()
-# r.recorded_data['type'] = "mouse_movement"
-# r.recorded_data['data'] = m.mouse_movement
-
-# print(r.recorded_data)
-
-# Schema for sending the data (keystroke)
-# post_1 = {
-#     "name": "Keystroke",
-#     "Keystroke": "H",
-#     "Date": "9/11/2021",
-#     "IP Address": "1.2.3.4",
-# }
-# Schema for sending the data (mouse action)
-# post_2 = {
-#     "name": "Mouse_Action",
-#     "coordinate": (200,402),
-#     "click": True,
-#     "date": "9/11/2021",
-#     "ip_address": "1.2.3.4",
-#     "mac_address": "39:0f:b2:29:48"
-# }
-
-# db = DataBase()
-# db.query_db("post", post_1, "")
-# print(db.query_db("find", post_1, ""))
+            print("first value stored: {0}\nstored items: {1}".format(
+                self.__mock_db[-1], len(self.__mock_db)))
