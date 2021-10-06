d896263910cba47e881c3d6edd6a0096858c9a31
Emmanuel Briones <emmanuelbriones@github.com>
2021-10-05 23:21:11 -0600

    wish me luck

--- view/components/result_table.py
+++ view/components/result_table.py
@@ -1,4 +1,6 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
+from main import attain as dbList
+from view.avert import selected
 
 class ResultTable:
 
@@ -134,3 +136,115 @@ class ResultTable:
 
 	def printwhatv(self):
 		print("whatever")
+
+	def startTableMouseAction(self):
+		# avert_result_table = QtWidgets.QTableWidget(table_result)
+		self.avert_result_table = avert_result_table
+		self.avert_result_table.setFrameShape(QtWidgets.QFrame.NoFrame)
+		self.avert_result_table.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+		self.avert_result_table.setAlternatingRowColors(True)
+		# self.avert_result_table.setRowCount(1)
+		self.avert_result_table.setObjectName("avert_result_table")
+		self.avert_result_table.setColumnCount(6)
+		item = QtWidgets.QTableWidgetItem()
+		self.avert_result_table.setVerticalHeaderItem(0, item)
+		item = QtWidgets.QTableWidgetItem()
+		self.avert_result_table.setHorizontalHeaderItem(0, item)
+		item = QtWidgets.QTableWidgetItem()
+		self.avert_result_table.setHorizontalHeaderItem(1, item)
+		item = QtWidgets.QTableWidgetItem()
+		self.avert_result_table.setHorizontalHeaderItem(2, item)
+		item = QtWidgets.QTableWidgetItem()
+		self.avert_result_table.setHorizontalHeaderItem(3, item)
+		item = QtWidgets.QTableWidgetItem()
+		self.avert_result_table.setHorizontalHeaderItem(4, item)
+		item = QtWidgets.QTableWidgetItem()
+		self.avert_result_table.setHorizontalHeaderItem(5, item)
+		# item = QtWidgets.QTableWidgetItem()
+		# item.setCheckState(QtCore.Qt.Unchecked)
+		# self.avert_result_table.setItem(0, 0, item)
+		# item = QtWidgets.QTableWidgetItem()
+		# self.avert_result_table.setItem(0, 1, item)
+		# item = QtWidgets.QTableWidgetItem()
+		# self.avert_result_table.setItem(0, 2, item)
+		# item = QtWidgets.QTableWidgetItem()
+		# self.avert_result_table.setItem(0, 3, item)
+		# item = QtWidgets.QTableWidgetItem()
+		# self.avert_result_table.setItem(0, 4, item)
+		# item = QtWidgets.QTableWidgetItem()
+		# self.avert_result_table.setItem(0, 5, item)
+
+
+		self.avert_result_table.horizontalHeader().setCascadingSectionResizes(True)
+		self.avert_result_table.horizontalHeader().setDefaultSectionSize(150)
+		self.avert_result_table.horizontalHeader().setHighlightSections(False)
+		self.avert_result_table.horizontalHeader().setSortIndicatorShown(True)
+		self.avert_result_table.horizontalHeader().setStretchLastSection(True)
+		self.avert_result_table.verticalHeader().setVisible(True)
+		self.avert_result_table.verticalHeader().setHighlightSections(True)
+
+		self.avert_result_table.setSortingEnabled(True)
+		item = self.avert_result_table.horizontalHeaderItem(0)
+		_translate = QtCore.QCoreApplication.translate
+		item.setText(_translate("MainWindow", "Timestamp"))
+		item = self.avert_result_table.horizontalHeaderItem(1)
+		item.setText(_translate("MainWindow", "Button"))
+		item = self.avert_result_table.horizontalHeaderItem(2)
+		item.setText(_translate("MainWindow", "Mouse Action"))
+		item = self.avert_result_table.horizontalHeaderItem(3)
+		item.setText(_translate("MainWindow", "Mouse Action Value"))
+		item = self.avert_result_table.horizontalHeaderItem(4)
+		item.setText(_translate("MainWindow", "Active Window"))
+		item = self.avert_result_table.horizontalHeaderItem(5)
+		item.setText(_translate("MainWindow", "Window Focus"))
+		# __sortingEnabled = self.avert_result_table.isSortingEnabled()
+		# self.avert_result_table.setSortingEnabled(False)
+		# item = self.avert_result_table.item(0, 1)
+		# item.setText(_translate("MainWindow", "10-06-26 02:31:29"))
+		# item = self.avert_result_table.item(0, 2)
+		# item.setText(_translate("MainWindow", "Video"))
+		# item = self.avert_result_table.item(0, 3)
+		# item.setText(_translate("MainWindow", "125.42.0.1"))
+		# item = self.avert_result_table.item(0, 4)
+		# item.setText(_translate("MainWindow", "89:28:B2:C6:55:19"))
+		# item = self.avert_result_table.item(0, 5)
+		# item.setText(_translate("MainWindow", "A sample artifact"))
+		# self.avert_result_table.setSortingEnabled(__sortingEnabled)
+
+		# return avert_result_table
+
+	def populateTableMouseAction(self):
+			# global attain
+			newAttain = dbList[selected]
+			self.avert_result_table.setRowCount(len(newAttain))
+			_translate = QtCore.QCoreApplication.translate
+			
+			item = QtWidgets.QTableWidgetItem()
+			self.avert_result_table.setItem(0, 0, item)
+			item = QtWidgets.QTableWidgetItem()
+			self.avert_result_table.setItem(0, 1, item)
+			item = QtWidgets.QTableWidgetItem()
+			self.avert_result_table.setItem(0, 2, item)
+			item = QtWidgets.QTableWidgetItem()
+			self.avert_result_table.setItem(0, 3, item)
+			item = QtWidgets.QTableWidgetItem()
+			self.avert_result_table.setItem(0, 4, item)
+			item = QtWidgets.QTableWidgetItem()
+			self.avert_result_table.setItem(0, 5, item)
+			item = self.avert_result_table.item(0, 0)
+			item.setText(_translate("MainWindow", newAttain['timestamp']))
+			item = self.avert_result_table.item(0, 1)
+			item.setText(_translate("MainWindow", newAttain['button']))
+			item = self.avert_result_table.item(0, 2)
+			position = newAttain['data']
+			if position['clicked']:
+				item.setText(_translate("MainWindow", "Click"))
+			else:
+				item.setText(_translate("MainWindow", "Movement"))
+			item = self.avert_result_table.item(0, 3)
+			item.setText(_translate("MainWindow", position['position']))
+			item = self.avert_result_table.item(0, 4)
+			item.setText(_translate("MainWindow", ""))
+			item = self.avert_result_table.item(0, 5)
+			item.setText(_translate("MainWindow", ""))
+			return
