92a5e693840841f2801a2c8f692f8f64e91a9880
kmamamori <kmamamori@miners.utep.edu>
2021-10-17 23:01:03 -0600

    Proper way to implement Thread (#57)

--- recorders/systemcall_recorder.py
+++ recorders/systemcall_recorder.py
@@ -7,29 +7,35 @@ from datetime import datetime, timedelta
 class SytemsCallRecorder(RecordedData, t.Thread):
 	def __init__(self, ):
 		RecordedData.__init__(self)
-		t.Thread.__init__(self)
 		self.reset_entrydata()
 		self.willRecord = False
+		# self.syscall_thread = t.Thread()
 
 
 	def setRecorder(self, bool_record):
 		if bool_record:
+			self.syscall_thread = t.Thread(target=self.run)
 			print("==== SYSTEM CALL RECORDING STARTING =====")
 			self.willRecord = True
 			s.Popen('service auditd start', shell=True, stdout=s.PIPE, stderr=s.PIPE)
+			self.syscall_thread.start()
 		else:
 			print("==== SYSTEM CALL RECORDING ENDING =====")
 			s.Popen('service auditd stop', shell=True, stdout=s.PIPE, stderr=s.PIPE)
 			self.willRecord = False
+			self.syscall_thread.join()
 
 
 	def run(self):
 		while True:
 			i = 0
 			while self.willRecord:
+				# self.syscall_thread = t.Thread()
+				# self.syscall_thread.start()
 				now = datetime.now()
 				start_formatted = (now+timedelta(seconds=-1)).strftime("%m/%d/%Y %H:%M:%S")
 				end_formatted = now.strftime("%m/%d/%Y %H:%M:%S")
+				# cmd = "ausearch -ts 10/17/2021 17:00:00 -te 10/17/2021 17:30:00 -i"
 				# cmd = "sudo ausearch -ts " + start_formatted + " -te " + end_formatted + " -i"
 				cmd = "ausearch -ts " + start_formatted + " -te " + end_formatted + " -i -m SYSCALL"
 				output = s.Popen(cmd, shell=True, stdout=s.PIPE, stderr=s.PIPE)
@@ -42,6 +48,7 @@ class SytemsCallRecorder(RecordedData, t.Thread):
 						self.insert_to_db(self._systemcall_data)
 				sleep(1)
 				i += 1 # checking counter
+				# self.syscall_thread.join()
 
 
 	def reset_entrydata(self):
@@ -76,3 +83,8 @@ class SytemsCallRecorder(RecordedData, t.Thread):
 			return 'Information Maintenance'
 		elif systemcall in ['pipe', 'shmget', 'mmap']:
 			return 'Communication'
+
+
+# scr = SytemsCallRecorder()
+# scr.setRecorder(True)
+# scr.start()
