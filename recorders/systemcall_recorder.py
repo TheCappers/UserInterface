30836b5043157650e0cfca2e6779b3381dace78c
kmamamori <kmamamori@miners.utep.edu>
2021-10-17 20:18:39 -0600

    Cleaned the code.

--- recorders/systemcall_recorder.py
+++ recorders/systemcall_recorder.py
@@ -1,11 +1,8 @@
-from os import system
-from typing import Match
 from recorders.recorded_data import RecordedData
 import subprocess as s
 from time import sleep
 import threading as t
 from datetime import datetime, timedelta
-from Database.Database import DataBase
 
 class SytemsCallRecorder(RecordedData, t.Thread):
 		def __init__(self, ):
@@ -13,48 +10,38 @@ class SytemsCallRecorder(RecordedData, t.Thread):
 			t.Thread.__init__(self)
 			self.reset_entrydata()
 			self.willRecord = False
-			self.isAutoRecord = False # get this value from config class
-			# self.setRecorder()
-			# self.initRecorder()
 
 
 		def setRecorder(self, bool_record):
 			if bool_record:
-				print("==== SYSTEM CALL RECORDIN STARTING =====")
+				print("==== SYSTEM CALL RECORDING STARTING =====")
 				self.willRecord = True
 				s.Popen('service auditd start', shell=True, stdout=s.PIPE, stderr=s.PIPE)
 			else:
-				print("==== SYSTEM CALL RECORDIN ENDING =====")
+				print("==== SYSTEM CALL RECORDING ENDING =====")
 				s.Popen('service auditd stop', shell=True, stdout=s.PIPE, stderr=s.PIPE)
 				self.willRecord = False
 
 
 		def run(self):
-			print("just run")
 			while True:
 				i = 0
 				while self.willRecord:
-					print(i)
 					now = datetime.now()
 					start_formatted = (now+timedelta(seconds=-1)).strftime("%m/%d/%Y %H:%M:%S")
 					end_formatted = now.strftime("%m/%d/%Y %H:%M:%S")
 					# cmd = "sudo ausearch -ts " + start_formatted + " -te " + end_formatted + " -i"
-					cmd = "sudo ausearch -ts " + start_formatted + " -te " + end_formatted + " -i -m SYSCALL"
-					print(cmd)
+					cmd = "ausearch -ts " + start_formatted + " -te " + end_formatted + " -i -m SYSCALL"
 					output = s.Popen(cmd, shell=True, stdout=s.PIPE, stderr=s.PIPE)
-					# print(output.stderr)
-					print(cmd)
 					for line in output.stdout.readlines():
-						print(line)
 						self.reset_entrydata()
 						curline = line.decode().split()
-						# print(curline)
 						if curline[0] == 'type=SYSCALL':
 							self.sysCallHandler(curline)
 							print(self._systemcall_data)
 							self.insert_to_db(self._systemcall_data)
 					sleep(1)
-					i += 1
+					i += 1 # checking counter
 
 
 		def reset_entrydata(self):
@@ -76,7 +63,7 @@ class SytemsCallRecorder(RecordedData, t.Thread):
 				elif parts[0] == 'syscall':
 					self._systemcall_data['data']['systemcall_name'] = parts[1]
 					self._systemcall_data['data']['systemcall_calltype'] = self.systemcalltypeIdentifier(parts[1]) #identify type
-			# print(curline)
+
 
 		def systemcalltypeIdentifier(self, systemcall):
 			if systemcall in ['fork', 'exit', 'wait']:
