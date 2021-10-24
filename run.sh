d8bac319d2865be10c0571be5082331c4ea893c2
Esteban Retana <eretana238@gmail.com>
2021-10-23 22:49:01 -0600

    Updated way to run Avert (#74)

---
+++ run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+rm -rf __pycache__
+rm -rf configuration/__pycache__
+rm -rf controller/__pycache__
+rm -rf Database/__pycache__
+rm -rf recorders/__pycache__
+rm -rf view/__pycache__
+rm -rf view/components/__pycache__
+rm -rf view/configuration_tab/__pycache__
+rm -rf view/home_tab/__pycache__
+rm -rf view/sync_tab/__pycache__
+rm -rf view/assets/__pycache__
+rm -rf .idea
+
+sudo apt update
+sudo apt install -y auditd wmctrl
+
+if [ ! -d "venv/" ]
+then
+    python3 -m venv venv
+fi
+
+source "venv/bin/activate"
+pip install -r requirements.txt
+
+if [ ! systemctl is-active --quiet mongod ]
+then systemctl start mongod
+fi
+
+clear
+python3 main.py
