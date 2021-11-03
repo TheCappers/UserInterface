7ffff5e44995d9b352fa86735a9e31d305512818
kmamamori <kmamamori@miners.utep.edu>
2021-11-02 22:08:21 -0600

    Added keystroke recorder into new system recorder

--- recorders/new_systemcal_recorder.py
+++ recorders/new_systemcal_recorder.py
@@ -1,16 +1,61 @@
-import psutil
+from pynput import keyboard
+from recorders.recorded_data import RecordedData
+
+class KeyboardRecorder(RecordedData):
+		def __init__(self, isAutoRecord):
+			RecordedData.__init__(self)
+			self.isAutoRecord = False
+			self._keystroke_data = self.get_recorded_data()
+			self._keystroke_data['name'] = "Keystroke"
+			self._keystroke_data['data'] = ''
+			self.listener =  keyboard.Listener()
+			self.startKeyboardRecording()
+
+		def startKeyboardRecording(self):
+			# ...or, in a non-blocking fashion:
+			if self.isAutoRecord:
+					return
+			self.isAutoRecord = True
+			self.listener = keyboard.Listener(
+				on_press=self.on_press
+			)
+			self.listener.start()
+
+		def stopKeyboardRecording(self):
+			if not self.isAutoRecord:
+					return
+			self.isAutoRecord = False
+
+		def checkrecording(self):
+			return self.isAutoRecord
+
+		def on_press(self, key):
+				if not self.checkrecording():
+					return False
+				self._keystroke_data['data'] = str(key).replace('\'', '')
+				self.insert_to_db(self._keystroke_data)
+				# print(format(key))
+				print(str(key))
+
+		def on_release(self, key):
+				# print('{0} released'.format(key))
+				if key == keyboard.Key.esc:
+						# Stop listener
+						return False
+
+# import psutil
 
 
-def find_procs_by_name(name):
-	"Return a list of processes matching 'name'."
-	ls = []
-	for p in psutil.process_iter(['name']):
-			if p.info['name'] == name:
-					ls.append(p)
-	return ls
+# def find_procs_by_name(name):
+# 	"Return a list of processes matching 'name'."
+# 	ls = []
+# 	for p in psutil.process_iter(['name']):
+# 			if p.info['name'] == name:
+# 					ls.append(p)
+# 	return ls
 
 	
-process = find_procs_by_name('qterminal')
-# process.cmdline()
-for p in process:
-	print(p.cmdline())
+# process = find_procs_by_name('qterminal')
+# # process.cmdline()
+# for p in process:
+# 	print(p.cmdline())
