5130443ffcad4abc2885a3a1e19e6af5ea9bd0e3
Emmanuel Briones <emmanuelbriones@github.com>
2021-11-23 15:16:30 -0700

    implementing delete tag

--- view/home_tab/home.py
+++ view/home_tab/home.py
@@ -355,8 +355,6 @@ class Home:
 		""" DetailedViewAccordion Start """
 		self.detailed_view_accordion = DetailedView()
 		self.toolBox_accordion.addItem(self.detailed_view_accordion.get_accordion(), "Detailed_View_Accordion")
-		if self.detailed_view_accordion.tag_table is not None:
-			self.detailed_view_accordion.tag_table.cellClicked.connect(self.exportRow)
 		""" DetailedViewAccordion End """
 
 		"""Visualization Start"""
